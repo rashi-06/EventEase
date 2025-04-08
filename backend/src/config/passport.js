c63dd11193f7f61dd7b87d8960066f5ed04b083e
rashi-06 <rashisharma18062001@gmail.com>
2025-04-08 23:21:54 +0530

    Added passport.js for Oauth implementation

---
+++ backend/src/config/passport.js
@@ -0,0 +1,48 @@
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import dotenv from "dotenv";
+import User from "../models/User.js";
+import generateToken from "../utils/jwt.js";
+
+dotenv.config();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        if (!user) {
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            password: "google-oauth", // Dummy password
+          });
+        }
+
+        const token = generateToken(user._id);
+        return done(null, { user, token });
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+// Serialize user into session
+passport.serializeUser((userData, done) => {
+  done(null, userData);
+});
+
+// Deserialize user from session
+passport.deserializeUser((userData, done) => {
+  done(null, userData);
+});
+
+export default passport;
