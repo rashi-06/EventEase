6e9fd3624b91cd82bc1b38f52af8ad225b0c9705
rashi-06 <rashisharma18062001@gmail.com>
2025-04-22 22:24:46 +0530

    code restructure

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # EventEase
+
+# 🎫 - Backend
+
+EventEase is an Event Ticket Booking System with a subscription model, built with Node.js, Express, and MongoDB. This backend provides secure APIs for user authentication, event management, bookings, payments, and subscriptions.
+
+---
+
+## 📦 Tech Stack
+
+- **Backend**: Node.js, Express.js
+- **Database**: MongoDB with Mongoose
+- **Authentication**: JWT, Google OAuth2 (Passport.js)
+- **State Management (Frontend)**: Redux Toolkit
+- **Caching**: Redis
+- **Payment Gateway**: Razorpay
+- **Validation**: express-validator
+- **Security**: bcryptjs, dotenv
+
+---
--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,11 +1,31 @@
-import Redis from "ioredis";
+// import Redis from "ioredis";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST || "127.0.0.1",
+//     port: process.env.REDIS_PORT || 6379,
+// });
+
+// redis.on("connect", () => console.log("Connected to Redis"));
+// redis.on("error", (err) => console.error("Redis Error:", err));
+
+// export default redis;
+
+import { createClient } from 'redis';
+
+const client = createClient({
+    username: 'default',
+    password: 'nV3dm7wTzwjFno50SjdfF43NaeVU9Cqq',
+    socket: {
+        host: 'redis-12891.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 12891
+    }
 });
 
-redis.on("connect", () => console.log("Connected to Redis"));
-redis.on("error", (err) => console.error("Redis Error:", err));
+client.on('error', err => console.log('Redis Client Error', err));
 
-export default redis;
+await client.connect();
+
+await client.set('foo', 'bar');
+const result = await client.get('foo');
+console.log(result)  // >>> bar
+
