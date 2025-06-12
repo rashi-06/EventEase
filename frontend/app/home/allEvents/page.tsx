96bcec1bf79cbd869f604e84b49a108193e9963c
rashi-06 <rashisharma18062001@gmail.com>
2025-06-12 22:36:49 +0530

    Created home page

---
+++ frontend/app/home/allEvents/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const allEvents = () => {
+  return (
+    <>
+        <div>allEvents</div>
+        <h1>Hello user , welcome back</h1>
+    </>
+  )
+}
+
+export default allEvents
